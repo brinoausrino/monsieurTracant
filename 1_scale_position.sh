f4ca8b1da52aaaabddc54612c7ef9c1590e4c5e0
Brian Eschrich <hi@brinoausrino.com>
2022-08-02 15:12:53 +0200

    add border to svg

--- 1_scale_position.sh
+++ 1_scale_position.sh
@@ -4,7 +4,7 @@ DY=$5
 if [[ $4 == -* ]] || [[ $5 == -* ]]; then DX="-- "$4; fi
 
 # scales an image and moves it to the correct position of the paper
-vpype read $1.svg scaleto $2 $3 linesimplify -t 1 linemerge -t 15 linesort reloop layout 70cmx90cm translate $DX $DY write $1_scaled.svg
+vpype read $1.svg scaleto $2 $3 linesimplify -t 1 linemerge -t 20 linesort reloop layout 70cmx90cm translate $DX $DY write $1_scaled.svg
 
 # create mask
 #vpype read $1_mask.svg scaleto $2 $3 linesimplify -t 1  linesort reloop layout 70cmx90cm translate $DX $DY write $1_scaled_mask.svg
--- 3_export_hpgl.sh
+++ 3_export_hpgl.sh
@@ -1,2 +1,5 @@
+# add borders
+vpype read $1.svg read "border.svg" write "$1.svg"
+#convert to hpgl
 vpype read $1.svg write --device hp7576a --page-size 70_90 --absolute "$1.hpgl"
 python removePenselectCommands.py $1.hpgl
